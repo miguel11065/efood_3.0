6655b2677d08889850ea4855126d7111d090e9c2
miguel11065 <miguel11065@gmail.com>
2023-12-01 02:18:16 -0300

    Corrigindo pequeno erro no routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Pratos from './pages/Pratos'
 
-const Rotas = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/restaurante/:id" element={<Pratos />} />
-  </Routes>
-)
+const Rotas = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurante/:id" element={<Pratos />} />
+    </Routes>
+  )
+}
 
 export default Rotas
